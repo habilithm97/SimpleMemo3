c24234e3b74333ff4fd3a59d470cacacc118e04a
habilithm97 <habinkim97@gmail.com>
2025-01-03 14:59:37 +0900

    Room DAO

--- app/src/main/java/com/example/simplememo3/room/Memo.kt
+++ app/src/main/java/com/example/simplememo3/room/Memo.kt
@@ -8,7 +8,7 @@ import androidx.room.PrimaryKey
  -DB의 테이블을 나타내는 데이터 클래스 (테이블의 스키마 정의)
  -테이블 구조와 1:1 매핑
  */
-@Entity(tableName = "memo_table")
+@Entity(tableName = "memos")
 data class Memo(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "content") var content: String
---
+++ app/src/main/java/com/example/simplememo3/room/MemoDao.kt
@@ -0,0 +1,33 @@
+package com.example.simplememo3.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+/* DAO (Data Access Object)
+ -DB 작업을 정의하는 인터페이스
+ -데이터 작업과 SQL 쿼리를 간단하게 정의
+ -Room이 DAO를 기반으로 쿼리의 구현체 자동 생성
+ */
+@Dao
+interface MemoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMemo(memo: Memo)
+
+    @Query("select * from memos order by id")
+    fun getAll(): Flow<List<Memo>>
+}
+/* suspend
+ -시간이 오래 걸릴 수 있는 작업 (Room DB) -> 비동기 처리
+ -일시 중단 가능 -> UI 스레드 차단x
+ */
+
+/* Flow
+ -비동기 데이터 스트림
+ -순차적 데이터 업데이트
+ -Cold 스트림 : 데이터를 요청하기 전까지 실행 안 함
+ -Backpressure : 데이터 방출 속도 조절
+ -> LiveData는 UI와 강하게 결합되어 있음
+ */
