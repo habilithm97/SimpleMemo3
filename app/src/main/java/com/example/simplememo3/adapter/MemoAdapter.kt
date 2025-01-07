3240e9f30509f0c2cf7984707e059b934b304c09
habilithm97 <habinkim97@gmail.com>
2025-01-07 13:47:35 +0900

    RecyclerView Adapter (ListAdapter)

---
+++ app/src/main/java/com/example/simplememo3/adapter/MemoAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.simplememo3.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.simplememo3.databinding.ItemMemoBinding
+import com.example.simplememo3.room.Memo
+
+class MemoAdapter : ListAdapter<Memo, MemoAdapter.MemoViewHolder>(DIFF_CALLBACK) {
+
+    inner class MemoViewHolder(private val binding: ItemMemoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(memo: Memo) {
+            binding.tvContent.text = memo.content
+        }
+    }
+
+    companion object {
+        // RecyclerView 성능 최적화를 위해 변경 사항만 업데이트
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Memo>() {
+            override fun areItemsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+                return oldItem.id == newItem.id
+            }
+            override fun areContentsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemoViewHolder {
+        val binding = ItemMemoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MemoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MemoViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
