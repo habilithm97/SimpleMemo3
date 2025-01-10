cbcf7ee58863d77a39b1a8eb0503343f19ffe687
habilithm97 <habinkim97@gmail.com>
2025-01-10 23:53:17 +0900

    소프트 키보드 자동으로 올리기

--- app/src/main/java/com/example/simplememo3/ui/fragment/MemoFragment.kt
+++ app/src/main/java/com/example/simplememo3/ui/fragment/MemoFragment.kt
@@ -1,10 +1,12 @@
 package com.example.simplememo3.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import com.example.simplememo3.databinding.FragmentMemoBinding
 import com.example.simplememo3.ui.activity.MainActivity
 
@@ -24,6 +26,14 @@ class MemoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         // MainActivity 툴바에 업 버튼 활성화
         (activity as? MainActivity)?.showUpButton(true)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.edtMemo.requestFocus()
+        val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.edtMemo, InputMethodManager.SHOW_IMPLICIT)
     }
 
     override fun onDestroyView() {
